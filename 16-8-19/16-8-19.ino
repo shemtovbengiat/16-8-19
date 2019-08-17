aea0d739a9642d6a80842a96cacceb3ae8ce25f5
shemtoov <shemtoov@kalcalibre.co.il>
2019-08-17 18:06:07 +0300

    Add project files.

---
+++ 16-8-19/16-8-19.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		_16_8_19.ino
+ Created:	8/17/2019 6:06:00 PM
+ Author:	shemtoov
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
